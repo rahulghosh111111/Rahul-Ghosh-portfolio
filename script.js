648f3c0a39b3e547cb947b3d3571fd361b0b954a
Rahul Ghosh <rahulghosh111111@gmail.com>
2024-10-03 21:50:48 +0530

    push

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#element', {
+    strings: ['Software Developer', 'Web Developer','Freelancer',],
+    typeSpeed: 70,
+    backSpeed: 10,
+    loop: true
+  });
+  var typed = new Typed('#element1', {
+    strings: ['Hi I am Rahul Ghosh. Here I will tell you about myself.Here you can know about my skills,experience and education.',],
+    typeSpeed: 40,
+    loop: true 
+  });
